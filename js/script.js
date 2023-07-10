be6cc7c7fb6db5725194cda0eded732cf5caa5a2
Alexander <joyunruban@gmail.com>
2023-07-10 21:28:05 +0300

    add mailer smooth scroll and pageup

--- js/script.js
+++ js/script.js
@@ -93,27 +93,10 @@ $(document).ready(function(){
     }).done(function() {
       $(this).find("imput").val("");
       $('#consultation, #order').fadeOut();
-      $('.overlay, #thanks').fadeIn(slow);
+      $('.overlay, #thanks').fadeIn();
       $('form').trigger('reset');
     });
     return false;
-  });
-
-  //smooth scroll and pageup
-
-  $(window).scroll(function() {
-    if($(this).scrollTop() > 1600) {
-      $('.pageup').fadeIn();      
-    } else {
-      $('.pageup').fadeOut();
-    }
-  });
-
-  $("a[href^='#']").click(function(){
-    const _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-    return false;
   })
-  
 });
 
